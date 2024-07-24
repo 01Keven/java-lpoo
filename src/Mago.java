1d301f265c9bbd783b9ff9b1c606135e0fb6c687
Keven <kevenchristiancode@gmail.com>
2024-07-23 21:55:56 -0300

    Remove diretório java-lpoo do índice

---
+++ src/Mago.java
@@ -0,0 +1,5 @@
+public class Mago extends Personagem{
+    public Mago(String nome, int nivel, int vida, int mana, int defesa, int dano) {
+        super(nome, nivel, vida, mana, defesa, dano);
+    }
+}
